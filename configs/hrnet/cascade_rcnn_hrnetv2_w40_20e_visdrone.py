507e4c67f73691254ce5e50083271f39fa0eedf4
ngonhi <ngonhi@grinnell.edu>
2021-06-30 03:26:57 +0000

    visdrone

---
+++ configs/hrnet/cascade_rcnn_hrnetv2_w40_20e_visdrone.py
@@ -0,0 +1,166 @@
+_base_ = [
+    '../_base_/datasets/visdrone_detection.py',
+    '../_base_/models/cascade_rcnn_r50_fpn.py',
+]
+# model settings
+model = dict(
+    pretrained='open-mmlab://msra/hrnetv2_w40',
+    backbone=dict(
+        _delete_=True,
+        type='HRNet',
+        extra=dict(
+            stage1=dict(
+                num_modules=1,
+                num_branches=1,
+                block='BOTTLENECK',
+                num_blocks=(4, ),
+                num_channels=(64, )),
+            stage2=dict(
+                num_modules=1,
+                num_branches=2,
+                block='BASIC',
+                num_blocks=(4, 4),
+                num_channels=(40, 80)),
+            stage3=dict(
+                num_modules=4,
+                num_branches=3,
+                block='BASIC',
+                num_blocks=(4, 4, 4),
+                num_channels=(40, 80, 160)),
+            stage4=dict(
+                num_modules=3,
+                num_branches=4,
+                block='BASIC',
+                num_blocks=(4, 4, 4, 4),
+                num_channels=(40, 80, 160, 320)))),
+    neck=dict(
+        _delete_=True,
+        type='HRFPN',
+        in_channels=[40, 80, 160, 320],
+        out_channels=256),
+    rpn_head=dict(
+        anchor_generator=dict(scales=[4])),
+    roi_head=dict(
+        type='CascadeRoIHead',
+        bbox_head=[
+            dict(type='Shared2FCBBoxHead',num_classes=10),
+            dict(type='Shared2FCBBoxHead',num_classes=10),
+            dict(type='Shared2FCBBoxHead',num_classes=10)]),
+    # model training and testing settings
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1,
+                gpu_assign_thr=200),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=0,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=[
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.5,
+                    neg_iou_thr=0.5,
+                    min_pos_iou=0.5,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                    gpu_assign_thr=200),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False),
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.6,
+                    neg_iou_thr=0.6,
+                    min_pos_iou=0.6,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                    gpu_assign_thr=200),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False),
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.7,
+                    min_pos_iou=0.7,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                    gpu_assign_thr=200),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False)
+        ]),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100)))
+#optimizer
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11, 15])
+runner = dict(type='EpochBasedRunner', max_epochs=20)
+work_dir='./work_dirs/cascade_rcnn_hrnetv2_w40_20e_visdrone.py'
+total_epochs=20
+
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
---
+++ tools/visdrone_detectoRS_train.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CONFIG=/home/ubuntu/nhi_workspace/mmdetection/configs/detectors/detectors_cascade_rcnn_r50_1x_visdrone.py
+GPUS=1
+PORT=${PORT:-29501}
+GPU_IDS=0
+CHECKPOINT=/home/ubuntu/nhi_workspace/mmdetection/work_dirs/detectors_cascade_rcnn_r50_1x_visdrone.py/latest.pth
+PYTHON=${PYTHON:-"python"}
+# $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#     tools/train.py $CONFIG --launcher pytorch ${@:3} --resume-from $CHECKPOINT
+$PYTHON tools/train.py $CONFIG --gpu-ids $GPU_IDS --resume-from $CHECKPOINT
