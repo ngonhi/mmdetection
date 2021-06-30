cb7bfdc5828222801d1296715a5c4a1d2df05e0c
ngonhi <ngonhi@grinnell.edu>
2021-06-30 10:43:37 +0000

    update detectors config

--- configs/detectors/detectors_cascade_rcnn_r50_1x_visdrone.py
+++ configs/detectors/detectors_cascade_rcnn_r50_1x_visdrone.py
@@ -209,7 +209,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=20)
 work_dir='./work_dirs/detectors_cascade_rcnn_r50_1x_visdrone.py'
 total_epochs=20
 
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=1, max_keep_ckpt=2)
 # yapf:disable
 log_config = dict(
     interval=50,
