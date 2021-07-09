db324c0079f0ce2771369ec1f84c4c4f2237126e
ngonhi <ngonhi@grinnell.edu>
2021-07-09 10:19:53 +0000

    config for hrnet

--- configs/hrnet/cascade_rcnn_hrnetv2_w40_20e_splitted_visdrone.py
+++ configs/hrnet/cascade_rcnn_hrnetv2_w40_20e_splitted_visdrone.py
@@ -37,37 +37,13 @@ model = dict(
         in_channels=[40, 80, 160, 320],
         out_channels=256),
     rpn_head=dict(
-        anchor_generator=dict(scales=[16],strides=[4, 8, 16, 32, 64], ratios=[0.33, 0.5, 1.0, 2.0, 3.0]),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0)),
+        anchor_generator=dict(scales=[4, 8, 16],strides=[4, 8, 16, 32, 64], ratios=[0.5, 1.0, 2.0])),
     roi_head=dict(
         type='CascadeRoIHead',
         bbox_head=[
-            dict(type='Shared2FCBBoxHead',num_classes=10, 
-                loss_cls=dict(
-                    type='FocalLoss',
-                    use_sigmoid=True,
-                    gamma=2.0,
-                    alpha=0.25,
-                    loss_weight=1.0)),
-            dict(type='Shared2FCBBoxHead',num_classes=10,
-                loss_cls=dict(
-                    type='FocalLoss',
-                    use_sigmoid=True,
-                    gamma=2.0,
-                    alpha=0.25,
-                    loss_weight=1.0)),
-            dict(type='Shared2FCBBoxHead',num_classes=10,
-                loss_cls=dict(
-                    type='FocalLoss',
-                    use_sigmoid=True,
-                    gamma=2.0,
-                    alpha=0.25,
-                    loss_weight=1.0))]),
+            dict(type='Shared2FCBBoxHead',num_classes=10),
+            dict(type='Shared2FCBBoxHead',num_classes=10),
+            dict(type='Shared2FCBBoxHead',num_classes=10)]),
     # model training and testing settings
     train_cfg=dict(
         rpn=dict(
@@ -158,6 +134,7 @@ model = dict(
             max_per_img=100)))
 #optimizer
 optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+# optimizer_config = dict(grad_clip=None)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -167,7 +144,7 @@ lr_config = dict(
     warmup_ratio=0.001,
     step=[8, 15])
 runner = dict(type='EpochBasedRunner', max_epochs=20)
-work_dir='./work_dirs/cascade_rcnn_hrnetv2_w40_20e_splitted_visdrone.py'
+work_dir='./work_dirs/cascade_rcnn_hrnetv2_w40_20e_splitted_visdrone_cross_entropy_3ar'
 total_epochs=20
 
 checkpoint_config = dict(interval=1, max_keep_ckpts=5)
@@ -183,6 +160,6 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
+load_from = './checkpoints/cascade_rcnn_hrnetv2p_w40_20e_coco_20200512_161112-75e47b04.pth'
 resume_from = None
 workflow = [('train', 1)]
